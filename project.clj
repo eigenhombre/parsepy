2c0dad7b794cc432c509d71a6bdc70e4e039d3fa
John Jacobsen <john@mail.npxdesigns.com>
2013-11-24 22:36:42 -0600

    Add license and URL

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject parsepy "0.0.1-SNAPSHOT"
   :description "Parse Python-style configuration files, in Clojure"
   :author "John Jacobsen, NPX Designs, Inc."
+  :license "MIT"
+  :url "https://github.com/eigenhombre/parsepy"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [instaparse "1.2.11"]])
